fe2c7c5a52f79484dbc79d23913dea7d9f4d1da7
bluemiv <public.bluemiv@gmail.com>
2025-03-20 22:56:47 +0900

    chore(tailwind): Configure TailwindCSS and apply Pretendard font

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // transpilePackages: ['next-mdx-remote'],
+  // images: {
+  //   remotePatterns: [{ protocol: 'https', hostname: 'bluemiv.github.io' }],
+  // },
 };
 
 export default nextConfig;

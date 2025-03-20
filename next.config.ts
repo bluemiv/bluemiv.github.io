e1de99f0a419c802ae0cef2cc7656a3acf8d5986
taehongkim <public.bluemiv@gmail.com>
2025-03-21 08:59:26 +0900

    chore(cache-control): Remove cache control

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
   // transpilePackages: ['next-mdx-remote'],
   // images: {
   //   remotePatterns: [{ protocol: 'https', hostname: 'bluemiv.github.io' }],

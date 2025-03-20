349a1daa14727feb8ce6248644d26a11bccfce1f
bluemiv <public.bluemiv@gmail.com>
2025-03-20 23:36:07 +0900

    feat(ui): Add Header component

---
+++ src/widgets/Header.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Link from 'next/link';
+import { ROUTE_PATH } from '@/shared/constants/route';
+
+export default function Header() {
+  return (
+    <header className="w-full px-md h-[50px] sticky top-0 z-10">
+      <nav className="max-w-[1280px] w-full h-full mx-auto flex items-center justify-between">
+        <Link href={ROUTE_PATH.ROOT}>Bluemiv</Link>
+        <ul className="flex gap-md">
+          {[
+            { href: ROUTE_PATH.ABOUT, label: 'ABOUT' },
+            { href: ROUTE_PATH.BLOG, label: 'BLOG' },
+          ].map(({ href, label }) => (
+            <li key={href}>
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}

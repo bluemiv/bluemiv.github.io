c182fad739ddcd8fef3ca20bd3ec0b917eaf8bcb
bluemiv <public.bluemiv@gmail.com>
2025-04-07 09:47:41 +0900

    refactor: ArticleMetadata 컴포넌트 분리

---
+++ src/entities/post/ui/ArticleMetadata.tsx
@@ -0,0 +1,28 @@
+import dayjs from 'dayjs';
+import ArticleThumbnail from './ArticleThumbnail';
+import { Post } from '../model';
+
+interface Props {
+  post: Post;
+}
+
+export default function ArticleMetadata({ post }: Props) {
+  return (
+    <div className="flex flex-col gap-md pb-2xl mb-lg">
+      {post.metadata.thumbnail && (
+        <ArticleThumbnail thumbnail={post.metadata.thumbnail} alt={post.metadata.title} />
+      )}
+      <h1 className="font-semibold text-4xl">{post.metadata.title}</h1>
+      <div className="flex flex-col gap-xs text-sm text-app-sub-text dark:text-app-dark-sub-text">
+        <div>{post.metadata.author}</div>
+        <div className="flex gap-md">
+          {post.metadata.updatedAt &&
+            !dayjs(post.metadata.createdAt).isSame(dayjs(post.metadata.updatedAt), 'minutes') && (
+              <div>최종 수정일: {dayjs(post.metadata.updatedAt).format('YYYY-MM-DD HH:mm:ss')}</div>
+            )}
+          <div>작성일: {dayjs(post.metadata.createdAt).format('YYYY-MM-DD HH:mm:ss')}</div>
+        </div>
+      </div>
+    </div>
+  );
+}

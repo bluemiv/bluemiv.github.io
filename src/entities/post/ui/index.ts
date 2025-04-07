204e9696024fc8e7717ec87bec6aa65a447d0f73
taehongkim <public.bluemiv@gmail.com>
2025-04-07 09:43:59 +0900

    refactor: ArticleThumbnail 컴포넌트 분리

--- src/entities/post/ui/index.ts
+++ src/entities/post/ui/index.ts
@@ -2,3 +2,4 @@ export { default as PostCard } from './PostCard';
 export { default as PostMdxContent } from './PostMdxContent';
 export { default as GridPostCard } from './GridPostCard';
 export { default as TableOfContent } from './TableOfContent';
+export { default as ArticleThumbnail } from './ArticleThumbnail';

fa399073cd2a0e2b03695612990f3d52ed641dc7
bluemiv <public.bluemiv@gmail.com>
2025-04-08 00:32:10 +0900

    feat: 홈 화면 Sidebar 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import { getAllPosts } from '@/entities/post/api';
 import { GridPostCard } from '@/entities/post/ui';
+import { Sidebar } from '@/widgets/Sidebar';
 
 export default function Home() {
   const posts = getAllPosts();
-  
+
   return (
-    <div className=" p-md">
-      <div className="mx-auto w-full max-w-[1280px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-md">
-        {posts.map((post) => (
-          <GridPostCard key={[post.metadata.category, post.metadata.slug].join('/')} post={post} />
-        ))}
+    <div>
+      <div className="flex items-start justify-start gap-md">
+        <Sidebar />
+        <div className="flex-1">
+          <main className="max-w-[1280px] p-md mx-auto w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-md">
+            {posts.map((post) => (
+              <GridPostCard
+                key={[post.metadata.category, post.metadata.slug].join('/')}
+                post={post}
+              />
+            ))}
+          </main>
+        </div>
       </div>
     </div>
   );

1edb580fb6495d72e3133a30f8c92ab2415152ac
taehongkim <public.bluemiv@gmail.com>
2025-04-08 15:39:15 +0900

    style: GridPostCard 컴포넌트 스타일 수정

--- src/entities/post/ui/GridPostCard.tsx
+++ src/entities/post/ui/GridPostCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-import { Post } from '@/entities/post/model';
 import Link from 'next/link';
+import dayjs from 'dayjs';
+import { Post } from '@/entities/post/model';
 
 interface Props {
   post: Post;
@@ -10,9 +11,9 @@ export default function GridPostCard({ post }: Props) {
   return (
     <Link
       href={`/blog/${[post.metadata.category, post.metadata.slug].join('/')}`}
-      className="animate-fade-in flex flex-col rounded-lg overflow-hidden transition-all ease-in-out duration-300 group cursor-pointer border border-app-sub-bg dark:border-app-dark-sub-bg"
+      className="h-[380px] animate-fade-in flex flex-col rounded-lg overflow-hidden transition-all ease-in-out duration-300 group cursor-pointer border border-app-sub-bg dark:border-app-dark-sub-bg"
     >
-      <div className="w-full h-[180px] bg-slate-100 overflow-hidden">
+      <div className="w-full min-h-[180px] max-h-[180px] bg-slate-100 overflow-hidden">
         {!!post.metadata.thumbnail && (
           <Image
             className="w-full h-full object-cover group-hover:scale-110 duration-150 ease-in-out"
@@ -23,12 +24,18 @@ export default function GridPostCard({ post }: Props) {
           />
         )}
       </div>
-      <div className="flex flex-col gap-md p-md">
-        <div className="font-semibold group-hover:text-app-primary dark:group-hover:text-app-dark-primary">
+      <div className="flex-1 flex flex-col gap-md p-md">
+        <div className="line-clamp-2 font-semibold group-hover:text-app-primary dark:group-hover:text-app-dark-primary">
           {post.metadata.title}
         </div>
-        <div className="line-clamp-2 text-app-sub-text dark:text-app-dark-sub-text">
+        <div className="line-clamp-3 text-app-sub-text dark:text-app-dark-sub-text">
           {post.metadata.description}
+        </div>
+        <div className="flex-1 text-app-sub-text dark:text-app-dark-sub-text text-sm flex items-end justify-between gap-sm">
+          <span className="bg-app-sub-bg dark:bg-app-dark-sub-bg px-xs rounded-md">
+            {post.metadata.category}
+          </span>
+          <span>{dayjs(post.metadata.createdAt).format('YYYY-MM-DD HH:mm')}</span>
         </div>
       </div>
     </Link>

f6d3fc00fa0c5ace373426a712faa0cc35955c17
bluemiv <public.bluemiv@gmail.com>
2025-04-08 22:16:35 +0900

    feat: Google Adsense 적용

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -27,6 +27,25 @@ export default function Sidebar() {
           })}
         </ul>
       </section>
+      <section>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9462926197232794"
+               crossorigin="anonymous"></script>
+          <ins class="adsbygoogle"
+               style="display:block"
+               data-ad-client="ca-pub-9462926197232794"
+               data-ad-slot="9216376708"
+               data-ad-format="auto"
+               data-full-width-responsive="true"></ins>
+          <script>
+               (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        `,
+          }}
+        />
+      </section>
     </aside>
   );
 }

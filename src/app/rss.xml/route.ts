cad57b181ea70ffc706007d5e7673521f236de89
bluemiv <public.bluemiv@gmail.com>
2025-04-10 16:44:52 +0900

    feat: rss.xml 추가

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -2,13 +2,24 @@ import { getAllPosts } from '@/entities/post/api';
 
 export async function GET() {
   const posts = getAllPosts();
-  const siteUrl = process.env.BASE_URL || 'https://bluemiv.github.io';
+  const siteUrl = process.env.BASE_URL;
+  const author = `${process.env.METADATA_EMAIL} (${process.env.METADATA_AUTHOR})`;
 
   const itemsXml = posts
-    .filter((post) => post.metadata.release) // 공개된 포스트만 포함
+    .filter((post) => post.metadata.release)
     .map((post) => {
       const { metadata } = post;
       const postUrl = `${siteUrl}/posts/${metadata.slug}`;
+
+      const categoriesXml = metadata.tags
+        .map((tag) => `<category><![CDATA[${tag}]]></category>`)
+        .join('');
+      const authorXml = `<author>${author}</author>`;
+      const thumbnailXml = metadata.thumbnail
+        ? `<enclosure url="${siteUrl}${metadata.thumbnail}" type="image/webp" />`
+        : '';
+      const contentXml = `<content:encoded><![CDATA[${post.content}]]></content:encoded>`;
+
       return `
   <item>
     <title><![CDATA[${metadata.title}]]></title>
@@ -16,17 +27,25 @@ export async function GET() {
     <guid>${postUrl}</guid>
     <description><![CDATA[${metadata.description}]]></description>
     <pubDate>${new Date(metadata.createdAt).toUTCString()}</pubDate>
+    ${authorXml}
+    ${categoriesXml}
+    ${thumbnailXml}
+    ${contentXml}
   </item>`;
     })
     .join('');
 
   const rssXml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
+<rss version="2.0"
+     xmlns:content="http://purl.org/rss/1.0/modules/content/"
+     xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
   <title>${process.env.METADATA_TITLE}</title>
   <link>${siteUrl}</link>
   <description>${process.env.METADATA_DESCRIPTION}</description>
   <language>ko</language>
+  <ttl>60</ttl>
+  <managingEditor>${author}</managingEditor>
   <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
   ${itemsXml}
 </channel>

14b6565bae58f4454c37f485b32db3f24c6df2cc
bluemiv <public.bluemiv@gmail.com>
2025-04-14 17:56:21 +0900

    feat: PWA 캐시 키 값 빌드시 자동으로 생성되로록 개선

--- src/features/serviceWorker/hooks/useRegisterServiceWorker.ts
+++ src/features/serviceWorker/hooks/useRegisterServiceWorker.ts
@@ -4,14 +4,7 @@ export default function useRegisterServiceWorker() {
   useEffect(() => {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker
-          .register('/sw.js')
-          .then((registration) => {
-            console.log('서비스 워커 등록 성공:', registration);
-          })
-          .catch((error) => {
-            console.error('서비스 워커 등록 실패:', error);
-          });
+        navigator.serviceWorker.register('/sw.js');
       });
     }
   }, []);

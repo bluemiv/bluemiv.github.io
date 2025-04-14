bcdb24263bc170d51d00891de8a46086d93811fe
bluemiv <public.bluemiv@gmail.com>
2025-04-14 15:03:54 +0900

    feat: Logo에 이미지 추가

--- src/widgets/Header/Logo.tsx
+++ src/widgets/Header/Logo.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
 import { ROUTE_PATH } from '@/shared/constants/route';
+import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <Link href={ROUTE_PATH.ROOT} className="font-semibold italic">
-      Bluemiv.
+    <Link href={ROUTE_PATH.ROOT} className="font-semibold italic flex items-center gap-xs">
+      <Image
+        src="/icon-192x192.png"
+        alt="Bluemiv"
+        width={50}
+        height={50}
+        className="min-w-[1.5rem] w-auto h-[1.5rem]"
+      />
+      <span className="hidden md:inline">Bluemiv.</span>
     </Link>
   );
 }

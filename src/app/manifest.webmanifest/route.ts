3467769a0a0a98a4a35d9e66da4d8f61ed107faf
taehongkim <public.bluemiv@gmail.com>
2025-04-15 16:44:51 +0900

    chore: manifest 파일 수정

--- src/app/manifest.webmanifest/route.ts
+++ src/app/manifest.webmanifest/route.ts
@@ -1,34 +1,33 @@
 export function GET() {
-  return new Response(
-    `{
-  "id": "/",
-  "name": ${process.env.METADATA_TITLE},
-  "short_name": ${process.env.METADATA_NICNAME},
-  "description": ${process.env.METADATA_DESCRIPTION},
-  "orientation": "portrait",
-  "lang": "ko",
-  "scope": "/",
-  "start_url": "/",
-  "display": "standalone",
-  "icons": [
-    {
-      "src": "/icon-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png",
-    },
-    {
-      "src": "/icon-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png",
-    },
-  ],
-}`,
-    {
-      headers: {
-        'Content-Type': 'application/manifest+json',
+  const manifest = {
+    id: '/',
+    name: process.env.METADATA_TITLE,
+    short_name: process.env.METADATA_NICNAME,
+    description: process.env.METADATA_DESCRIPTION,
+    orientation: 'portrait',
+    lang: 'ko',
+    scope: '/',
+    start_url: '/',
+    display: 'standalone',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+
+  return new Response(JSON.stringify(manifest), {
+    headers: {
+      'Content-Type': 'application/manifest+json',
     },
-  );
+  });
 }
 
 export const dynamic = 'force-static';

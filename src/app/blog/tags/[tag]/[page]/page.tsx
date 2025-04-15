e7100a035d2cf2918240242f493c39b80c584edb
bluemiv <public.bluemiv@gmail.com>
2025-04-15 15:50:00 +0900

    feat: tag 페이지 개발

---
+++ src/app/blog/tags/[tag]/[page]/page.tsx
@@ -0,0 +1,53 @@
+import { getPageNumberByTag, getPostsByTag, getTags } from '@/entities/post/api';
+import { BlogHomeLayout } from '@/shared/layouts';
+import { LIMIT } from '@/shared/constants/pagination';
+
+interface Props {
+  params: Promise<{ tag: string; page: string }>;
+}
+
+export async function generateMetadata({ params }: Props) {
+  const { tag } = await params;
+  const common = {
+    title: `'${tag}'에 대한 글`,
+    description: `'${tag}'에 대한 글 목록입니다.`,
+  };
+  return {
+    ...common,
+    openGraph: {
+      ...common,
+    },
+  };
+}
+
+export default async function Page(props: Props) {
+  const { tag, page } = await props.params;
+  const pageNum = Number(page);
+  const posts = getPostsByTag(tag, { offset: (pageNum - 1) * LIMIT, limit: LIMIT });
+
+  return (
+    <BlogHomeLayout
+      mainAreaTitle={`'#${tag}'에 대한 글 (${page} 페이지)`}
+      posts={posts}
+      currentPageNum={pageNum}
+      totalPageNum={getPageNumberByTag(tag)}
+    />
+  );
+}
+
+export async function generateStaticParams() {
+  const tags = getTags();
+  return tags.reduce((acc: { tag: string; page: string }[], entry) => {
+    const tag = entry[0];
+    const totalPageNum = getPageNumberByTag(tag);
+    return [
+      ...acc,
+      ...Array.from({ length: totalPageNum }, (_, idx) => idx + 1).map((page) => ({
+        tag,
+        page: page.toString(),
+      })),
+    ];
+  }, []);
+}
+
+export const dynamic = 'force-static';
--- src/features/post/ui/Tag.tsx
+++ src/features/post/ui/Tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ tag }: Props) {
   return (
     <Link
       className="text-sm bg-app-sub-bg dark:bg-app-dark-sub-bg px-sm py-xs rounded-full duration-150 ease-in-out hover:shadow-md"
-      href={[ROUTE_PATH.BLOG_TAGS, tag].join('/')}
+      href={[ROUTE_PATH.BLOG_TAGS, tag, '1'].join('/')}
     >
       #{tag}
     </Link>

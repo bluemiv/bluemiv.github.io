915c031b17266e6c85843438624401b55e4c2deb
taehongkim <public.bluemiv@gmail.com>
2025-04-15 15:05:19 +0900

    feat: 홈 화면에 애드센스 적용

--- src/widgets/Sidebar/SectionTitle.tsx
+++ src/widgets/Sidebar/SectionTitle.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function SectionTitle({ children }: PropsWithChildren) {
   return (
-    <h6 className="text-sm text-app-sub-text dark:text-app-dark-sub-text font-semibold mb-sm">
+    <h6 className="text-sm text-app-sub-text dark:text-app-dark-sub-text font-semibold mb-md">
       {children}
     </h6>
   );

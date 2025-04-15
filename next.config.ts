0a2548635be02d5143b8a35a230393b5acc28e4d
bluemiv <public.bluemiv@gmail.com>
2025-04-15 16:15:51 +0900

    chore: .nojekyll 파일 추가

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from 'next';
-import dayjs from 'dayjs';
-
-const buildTime = dayjs().toISOString().replace(/[:.]/g, '-');
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   transpilePackages: ['next-mdx-remote'],
-  env: {
-    NEXT_PUBLIC_BUILD_TIME: buildTime,
-  },
 };
 
 export default nextConfig;

3127d18e753ce4609c3323fe5862d305f3653c13
bluemiv <public.bluemiv@gmail.com>
2025-04-15 16:34:20 +0900

    chore: url 띄어쓰기에 대한 방어로직으로 encodeURIComponent 추가

--- src/app/blog/category/[category]/[page]/page.tsx
+++ src/app/blog/category/[category]/[page]/page.tsx
@@ -43,7 +43,7 @@ export async function generateStaticParams() {
     return [
       ...acc,
       ...Array.from({ length: totalPageNum }, (_, idx) => idx + 1).map((page) => ({
-        category: encodeURIComponent(category),
+        category,
         page: page.toString(),
       })),
     ];

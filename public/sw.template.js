392a3c04c3dba3edd19538bddb9509d8dce2148a
taehongkim <public.bluemiv@gmail.com>
2025-04-21 15:38:55 +0900

    feat: sw.js 개선

--- public/sw.template.js
+++ public/sw.template.js
@@ -6,7 +6,8 @@ async function cleanupCaches() {
     cacheNames.map(async (cacheName) => {
       if (CACHE_NAME !== cacheName) {
         console.log('[SW:fetch] 새로운 컨텐츠 감지됨 → postMessage & 캐시 정리');
-        clients.forEach((client) => client.postMessage({ type: 'NEW_VERSION_AVAILABLE' }));
+        const allClients = await self.clients.matchAll();
+        allClients.forEach((client) => client.postMessage({ type: 'NEW_VERSION_AVAILABLE' }));
         return caches.delete(cacheName);
       }
     }),

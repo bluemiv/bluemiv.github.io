a4fe7f42280ddacd96778e050d9742decc327a62
bluemiv <public.bluemiv@gmail.com>
2025-04-21 15:41:10 +0900

    style: pagination 간격 추가

--- src/shared/layouts/BlogHomeLayout.tsx
+++ src/shared/layouts/BlogHomeLayout.tsx
@@ -41,12 +41,12 @@ export default function BlogHomeLayout({
           <div className="overflow-hidden w-full">
             <ResponsiveAd />
           </div>
-          <div className="w-full flex items-center justify-center">
+          <div className="w-full flex items-center justify-center gap-md">
             {Array.from({ length: totalPageNum }, (_, idx) => idx + 1).map((page) => (
               <div
                 key={page}
                 className={clsx(
-                  ' w-[32px] h-[32px] flex items-center justify-center gap-md rounded-md',
+                  ' w-[32px] h-[32px] flex items-center justify-center rounded-md',
                   page === currentPageNum
                     ? 'bg-app-primary dark:bg-app-dark-primary text-white cursor-not-allowed'
                     : 'bg-app-sub-bg dark:bg-app-dark-sub-bg hover:bg-app-primary dark:hover:bg-app-dark-primary hover:text-white duration-100 ease-in-out cursor-pointer',

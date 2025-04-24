b81dc9712180918bedad1d47718c2a04860b9bfa
bluemiv <public.bluemiv@gmail.com>
2025-04-24 22:06:48 +0900

    feat: short 글 개발

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -13,6 +13,7 @@ export default function Header() {
           {[
             // { href: ROUTE_PATH.ABOUT, label: 'ABOUT' },
             { href: ROUTE_PATH.ROOT, label: 'HOME' },
+            // { href: ROUTE_PATH.BLOG_SHORT, label: 'SHORT' },
             // { href: ROUTE_PATH.BLOG_TAGS, label: 'TAGS' },
           ].map(({ href, label }) => (
             <li key={href}>

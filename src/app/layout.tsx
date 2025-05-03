ea3744e49d4b08f388e946d7982e0c38f7c1fff2
taehongkim <public.bluemiv@gmail.com>
2025-05-03 16:53:31 +0900

    chore: GA 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,13 @@ export default function RootLayout({
     <html lang="ko" suppressHydrationWarning>
       <body className={`${pretendardFont.className} antialiased min-h-screen`}>
         {/*Google Analytics*/}
-        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
-        <script
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}

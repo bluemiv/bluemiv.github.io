4f0e1250d6644d497cc1d2864cb3cb4d97475f1b
taehongkim <public.bluemiv@gmail.com>
2025-05-30 16:36:26 +0900

    feat: 다크 테마 전역으로 분리 및 본문 댓글 기능 추가

---
+++ src/entities/post/ui/Comments.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { useTheme } from '@/features/toggleTheme/hooks';
+
+export const Comments = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    if (!ref.current) return;
+    ref.current.innerHTML = '';
+
+    const script = document.createElement('script');
+    script.src = 'https://utteranc.es/client.js';
+    script.setAttribute('repo', process.env.NEXT_PUBLIC_COMMENTS_REPO!);
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('theme', theme === 'light' ? 'github-light' : 'dark-blue');
+    script.setAttribute('cross-origin', 'anonymous');
+    script.setAttribute('async', '');
+    ref.current?.appendChild(script);
+  }, [theme]);
+
+  return (
+    <div ref={ref} className="w-full min-h-[300px] [&_.utterances_.markdown-body]:bg-transparent" />
+  );
+};

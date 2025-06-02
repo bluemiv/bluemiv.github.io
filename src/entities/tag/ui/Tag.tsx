57041245aa4b71ce986deab78850719e5839d397
bluemiv <public.bluemiv@gmail.com>
2025-06-02 15:00:35 +0900

    style: Tag 컴포넌트 스타일 변경

--- src/entities/tag/ui/Tag.tsx
+++ src/entities/tag/ui/Tag.tsx
@@ -1,11 +1,14 @@
+import { TagIcon } from '@/shared/ui/Icons';
+
 interface Props {
   tag: string;
 }
 
 export const Tag = ({ tag }: Props) => {
   return (
-    <span className="inline-block text-sm bg-app-sub-bg dark:bg-app-dark-sub-bg px-sm py-xs rounded-full">
-      #{tag}
+    <span className="text-sm bg-app-sub-bg dark:bg-app-dark-sub-bg px-sm py-xs rounded-md flex items-center gap-xs">
+      <TagIcon className="w-[0.8rem] " />
+      {tag}
     </span>
   );
 };
--- src/shared/ui/Icons/RSSIcon.tsx
+++ src/shared/ui/Icons/RSSIcon.tsx
@@ -1,6 +1,6 @@
 import { PropsWithClassName } from '@/shared/types/props';
 
-export default function RSSIcon({ className }: PropsWithClassName) {
+export const RSSIcon = ({ className }: PropsWithClassName) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,4 +11,4 @@ export default function RSSIcon({ className }: PropsWithClassName) {
       <path d="M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1-1-1m0 4a1 1 0 0 1 1-1 6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1-1-1" />
     </svg>
   );
-}
+};

b4ea28d77558c513f1f20cfb9dd1765dd12f73ed
bluemiv <public.bluemiv@gmail.com>
2026-02-11 22:52:32 +0900

    refactor: FSD 구조에서 feature 기반 구조로 변경

--- src/entities/post/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as PostMdxContent } from './PostMdxContent';
-export { default as GridPostCard } from './GridPostCard';
-export { default as TableOfContent } from './TableOfContent';
-export { default as ArticleMetadata } from './ArticleMetadata';
-export * from './CategoryTag';
-export * from './NextOrPrevPostCard';
-export * from './Pagination';
-export * from './Comments';

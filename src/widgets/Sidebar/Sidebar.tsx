53824a942124193ae6e0894680ce0735f01af7e4
bluemiv <public.bluemiv@gmail.com>
2026-05-16 23:15:39 +0900

    fix: keep sidebar divider continuous

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { TagSection } from './TagSection';
 
 export default function Sidebar() {
   return (
-    <aside className="motion-stagger min-w-[264px] w-[264px] hidden lg:flex px-md py-lg sticky top-[56px] left-0 overflow-x-hidden border-r border-app-border/80 dark:border-app-dark-border/80 h-[calc(100vh-56px)] overflow-hidden hover:overflow-y-auto flex-col gap-xl bg-app-bg dark:bg-app-dark-bg [scrollbar-gutter:stable]">
+    <aside className="motion-stagger relative min-w-[264px] w-[264px] hidden lg:flex px-md py-lg sticky top-[56px] left-0 overflow-x-hidden h-[calc(100vh-56px)] overflow-hidden hover:overflow-y-auto flex-col gap-xl bg-app-bg dark:bg-app-dark-bg [scrollbar-gutter:stable] before:pointer-events-none before:fixed before:left-[263px] before:top-0 before:bottom-0 before:w-px before:bg-app-border/80 dark:before:bg-app-dark-border/80">
       <ProfileSection />
       <CategorySection />
       <TagSection />

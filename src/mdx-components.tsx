5300a0cdf244edc5585817fb06df243e5aeee3b8
bluemiv <public.bluemiv@gmail.com>
2026-08-07 17:37:48 +0900

    fix: 코드 블록 헤더와 본문 겹침 수정

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,27 @@
+import type { ComponentPropsWithoutRef } from "react";
+
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
 
 import { getArticleImageDimensions } from "@/features/article/articleImage";
 
+type PropsWithArticleCodeBlock = ComponentPropsWithoutRef<"pre"> & {
+  "data-language"?: string;
+};
+
+function ArticleCodeBlock({ children, ...props }: PropsWithArticleCodeBlock) {
+  const language = props["data-language"];
+
+  return (
+    <pre {...props}>
+      {language ? <span className="article-code-language">{language}</span> : null}
+      {children}
+    </pre>
+  );
+}
+
 const ARTICLE_MDX_COMPONENTS = {
+  pre: ArticleCodeBlock,
   img: ({ src, alt, title }) => {
     if (typeof src !== "string") throw new Error("Article image src must be a string");
 
